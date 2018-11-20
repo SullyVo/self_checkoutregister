a70af3a05195ae7464d5af43d38adc4cdb1a71fc
SullyVo <45184954+SullyVo@users.noreply.github.com>
2018-11-19 16:08:08 -0800

    Add files via upload

---
+++ Hw3_A.cpp
@@ -0,0 +1,89 @@
+/**
+PART B
+Write a program that acts as a self-checkout register at a store and lets the user buy 2 items.
+
+The program will prompt the user and read in:
+- the name of item 1, such as mechanical pencil.
+- the price of item 1 and number of items 1, such as: 2 2.50
+- the name of item 2, such as  binder
+- the price of item 2 and the number of items 2, such as:  3 7.25
+Read both the price and the number in one C++ statement
+Then the program calculates the total cost of the items, including the sales tax of 8.675%.
+The tax should be defined as a constant in the program, and not used as a literal.
+The program prints the receipt on screen. The output should be in column format,
+and the prices are with 2 digits after the decimal point. You can assume the item names
+will not be more than 20 characters wide.
+
+
+
+
+Uyen Vo
+*/
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+string item1, item2,itemsAll;
+int item1Amount,item2Amount;
+double item1Price, item2Price;
+const double TAX = 0.08675;
+
+double item1Total, item2Total, items, itemsTotal;
+
+cout <<"Item 1 is mechanical pencil" <<endl;
+cout << "Enter the amount of item 1:\t" ;
+cin >> item1Amount;
+cout << "Enter the price of item 1:\t" ;
+cin >>item1Price;
+
+item1Total = item1Amount * item1Price;
+cout << "Total of item 1 is" << setprecision(2) << fixed << setw(8) <<item1Total <<endl;
+
+cout << "Item 2 is binder" <<endl;
+cout << "Enter the amount of item 2:\t" ;
+cin >> item2Amount;
+cout << "Enter the price of item 2:\t" ;
+cin >>item2Price;
+
+item2Total = item2Amount * item2Price;
+cout << "Total of item 2 is" <<setprecision(2) << fixed  << setw(8) <<item2Total <<endl;
+
+
+items = item1Total + item2Total;
+itemsTotal = (items * TAX) + items;
+cout << "Total of all items are" <<setprecision(2) << fixed <<setw(8) <<itemsTotal <<endl;
+
+
+    return 0;
+}
+/***********************************************************************
+SAVE THE FIRST OUTPUT HERE:
+mechanical pencil, 3 at 2.50 each
+binder, 5 at 7.25 each
+
+Item 1 is mechanical pencil
+Enter the amount of item 1:	3
+Enter the price of item 1:	2.50
+Total of item 1 is    7.50
+Item 2 is binder
+Enter the amount of item 2:	5
+Enter the price of item 2:	7.25
+Total of item 2 is   36.25
+Total of all items are   47.55
+
+/***********************************************************************
+SAVE THE SECOND OUTPUT HERE (input of your choice)
+Item 1 is mechanical pencil
+Enter the amount of item 1:	2
+Enter the price of item 1:	3.0
+Total of item 1 is    6.00
+Item 2 is binder
+Enter the amount of item 2:	3
+Enter the price of item 2:	5.0
+Total of item 2 is   15.00
+Total of all items are   22.82
+*/
---
+++ Hw3_B.cpp
@@ -0,0 +1,71 @@
+/**
+PART C
+Write a program that will do the following steps:
+1.	generate 5 random numbers between 3 and 13 inclusive
+ // r1, r2, r3, r4, r5
+2.	generate one random number between 2 and 6 // name it x
+3.	calculate r1 to the power of x
+4.	calculate the average of the 5 numbers
+5.	calculate the square root of the second and the last number
+6.	calculate the geometric mean of the second, third, and fourth number (the product of the three numbers to the power of 1 / 3)
+7.	print the 5 random numbers
+8.	print r1 to the power of x
+9.	print the average of the 5 numbers with 1 digit after the decimal point
+10.	print the square root of the second and last random number with 3 digits after the decimal point
+11.	print the geometric mean of the second, third, and fourth number with 3 digits after the decimal point
+
+
+Uyen Vo
+
+*/
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+srand(time(0));
+
+int r1, r2, r3, r4, r5, x;
+r1 = rand() % 11 + 3;
+r2 = rand() % 11 + 3;
+r3 = rand() % 11 + 3;
+r4 = rand() % 11 + 3;
+r5 = rand() % 11 + 3;
+x = rand() % 5 + 2;
+
+cout << "5 random numbers are:" <<r1 <<" " <<r2 <<" " <<r3 <<" " <<r4 <<" " <<r5 <<endl;
+cout << "The first number to the power of the random number x is:" << pow(r1,x) <<endl;
+cout <<"The average of the 5 numbers is:" <<setprecision(2) << (r1+r2+r3+r4+r5)/5.0 <<endl;
+
+cout << "The square root of the second number is:" <<setprecision(4) <<sqrt(r2) <<endl;
+cout << "The square root of the last number is:" <<setprecision(4) <<sqrt(r5) <<endl;
+cout << "The geometric mean of the second, third, and fourth number is:" <<setprecision(4) << pow(r2 * r3 * r4,1.000/3) <<endl;
+
+   return 0;
+}
+/*********************************************************************************
+SAVE THE FIRST OUTPUT HERE
+
+5 random numbers are:9 6 7 8 12
+The first number to the power of the random number x is:81
+The average of the 5 numbers is:8.4
+The square root of the second number is:2.449
+The square root of the last number is:3.464
+The geometric mean of the second, third, and fourth number is:6.952
+*/
+
+/*********************************************************************************
+SAVE THE SECOND OUTPUT HERE
+
+5 random numbers are:6 13 4 5 11
+The first number to the power of the random number x is:46656
+The average of the 5 numbers is:7.8
+The square root of the second number is:3.606
+The square root of the last number is:3.317
+The geometric mean of the second, third, and fourth number is:6.383
+
+*/
